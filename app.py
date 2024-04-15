ed7172e11ebf801a4ce6bacfa714dc3850f5137d
IMMontoya <Immontoya93@gmail.com>
2024-04-14 17:05:16 -0700

    First Half working

--- app.py
+++ app.py
@@ -42,58 +42,106 @@ df.rename(columns={'model_year': 'year', 'fuel': 'fuel_type',
 df = df[['price', 'make', 'model',
          'year', 'condition', 'fuel_type', 'odometer_miles']]
 
+# Change the year column to an integer
+df['year'] = df['year'].astype(int)
+
+# ---------------------------------#
+# Title
+st.title('Temp Title')
 # ---------------------------------#
 
+# ---------------------------------#
+# Header
+st.header('Price Distribution for Selected Vehicle')
 
-def update_model_list():
-    if selected_manufacturers:
-        filtered_df = df[df['make'].isin(selected_manufacturers)]
-        return filtered_df['model'].unique().tolist()
-    return []
+# Create three columns for the necessary arguments: make, model, and year
+arg_col1, arg_col2, arg_col3 = st.columns(3)
+
+with arg_col1:
+    # Create a select box for the manufacturers
+    manufacturers_list = df['make'].unique()
+    manufacturer = st.selectbox('Select a manufacturer', manufacturers_list)
+    # Filter the data
+    filtered_df = df[df['make'] == manufacturer]
+
+with arg_col2:
+    # Create a select box for the models
+    model_list = filtered_df['model'].unique()
+    model = st.selectbox('Select a model', model_list)
+    # Filter the data
+    filtered_df = filtered_df[filtered_df['model'] == model]
+
+with arg_col3:
+    # Create a select box for the years
+    year_list = filtered_df['year'].unique()
+    year = st.selectbox('Select a year', year_list)
+    # Filter the data
+    filtered_df = filtered_df[filtered_df['year'] == year]
+
+# Options for more detailed filtering
+opt_arg_col1, opt_arg_col2, opt_arg_col3 = st.columns(3)
+
+with opt_arg_col1:
+    # Create a checkbox for the condition
+    condition_chbox = st.checkbox('Specify the Condition?')
+    if condition_chbox:
+        condition_list = filtered_df['condition'].unique()
+        condition = st.selectbox('Select a condition', condition_list)
+        # Filter the data
+        filtered_df = filtered_df[filtered_df['condition'] == condition]
+
+with opt_arg_col2:
+    # Create a checkbox for the fuel type
+    fuel_chbox = st.checkbox('Specify the Fuel Type?')
+    if fuel_chbox:
+        fuel_list = filtered_df['fuel_type'].unique()
+        fuel = st.selectbox('Select a fuel type', fuel_list)
+        # Filter the data
+        filtered_df = filtered_df[filtered_df['fuel_type'] == fuel]
+
+with opt_arg_col3:
+    # Create a checkbox for the odometer
+    odometer_chbox = st.checkbox('Specify the Milage?')
+    if odometer_chbox:
+        # Create a slider for the odometer
+        min_odometer = int(filtered_df['odometer_miles'].min())
+        max_odometer = int(filtered_df['odometer_miles'].max())
+        odometer = st.slider('Select an odometer range', min_odometer,
+                             max_odometer, (min_odometer, max_odometer))
+        # Filter the data
+        filtered_df = filtered_df[(filtered_df['odometer_miles'] >= odometer[0]) & (
+            filtered_df['odometer_miles'] <= odometer[1])]
 
 
-def update_manufacturer_list():
-    if selected_models:
-        filtered_df = df[df['model'].isin(selected_models)]
-        return filtered_df['make'].unique().tolist()
-    return []
+# Reset the index
+filtered_df = filtered_df.reset_index(drop=True)
+
+# Get the stats for the selection
+stats = filtered_df['price'].describe()
+# Remove the std since it is not useful
+stats = stats.drop('std')
+# Convert the stats to a dataframe
+stats = stats.to_frame()
+# Add a description column
+stats['Description'] = ['# of matching vehicles in dataset', 'Average price',
+                        'Minimum price', '25th percentile', 'Median price', '75th percentile', 'Maximum price']
+# Re-arrange the columns
+stats = stats[['Description', 'price']]
+# Rename the price column
+stats.rename(columns={'price': 'Value'}, inplace=True)
+# Format everything but the count index to be in dollars
+stats['Value'] = stats['Value'].apply(
+    lambda x: f'${x:,.2f}' if x != stats.loc['count', 'Value'] else x)
+
+# Create a histogram of the prices
+price_dist_fig = px.histogram(filtered_df, x='price',
+                              histnorm='percent', template='plotly_dark', labels={'price': 'Price in USD'})
 
 
-# Title and header
-st.title('Dynamic Filtering Example')
-st.header('Price Distribution')
+# Show the histogram
+st.plotly_chart(price_dist_fig)
 
-# Manufacturers widget
-all_manufacturers = st.checkbox('All Manufacturers', key='all_manufacturers')
-manufacturer_list = df['make'].unique().tolist()
+# Show the stats
+st.write("Here are the stats for your selection:", stats)
 
-if all_manufacturers:
-    selected_manufacturers = manufacturer_list
-else:
-    selected_manufacturers = st.multiselect(
-        'Select Manufacturers',
-        options=manufacturer_list,
-        default=manufacturer_list if all_manufacturers else []
-    )
-
-# Update the model list based on selected manufacturers
-model_list = update_model_list()
-
-# Models widget
-all_models = st.checkbox('All Models', key='all_models')
-if all_models:
-    selected_models = model_list
-else:
-    selected_models = st.multiselect(
-        'Select Models',
-        options=model_list,
-        default=model_list if all_models else []
-    )
-
-# Update the manufacturer list based on selected models
-# This should be carefully handled to avoid conflicts; you might choose to skip this step or ensure it does not cause infinite loops
-# manufacturer_list = update_manufacturer_list()
-
-# Display selections
-# st.write("Selected Manufacturers:", selected_manufacturers)
-# st.write("Selected Models:", selected_models)
+# ---------------------------------#
